2b8c91d0840bc1a06b7833a05e87e032fb7940e4
Lucas <eve005@einstein.com>
2025-03-18 14:26:07 -0300

    estruturacao

--- portfolio-lucas-dev/src/app/app-routing.module.ts
+++ portfolio-lucas-dev/src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'portfolio',
+    pathMatch: 'full',
+  },
+  {
+    path: 'portfolio',
+    component: LayoutComponent,
+    // children: [
+    //   {
+    //     path: 'boas-vindas',
+    //     loadChildren: () =>
+    //       import('./featu').then(
+    //         (m) => m.BoasVindasModule
+    //       ),
+    //   },
+    // ],
+  },
+  /** Rota Coringa */
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
